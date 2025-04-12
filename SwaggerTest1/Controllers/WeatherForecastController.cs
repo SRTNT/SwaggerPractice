69bcd8db73da299d0d90d9aa55751dce6027beae
SRT Mokaab <s.r.taheri@gmail.com>
2025-04-12 15:42:25 +0330

    Describe With Summery And XML Structure

---
+++ SwaggerTest1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerTest1.Controllers
+{
+    /// <summary>
+    /// Controller to handle weather forecast related requests.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeatherForecastController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets the weather forecast.
+        /// </summary>
+        /// <returns>List of weather forecasts.</returns>
+        /// <response code="200">Returns the list of weather forecasts.</response>
+        /// <response code="404">If the weather forecast is not found.</response>
+        /// <response code="500">Internal server error.</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<WeatherForecast>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ SwaggerTest1/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwaggerTest1;
+using System.Reflection;
+
+[assembly: ApiConventionType(typeof(DefaultApiConventions))]
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("SRT", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "SRT API",
+        Version = "1",
+        Description = "SRT API Descriptions",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Email = "SRT@Test.com",
+            Name = "SRT Contract",
+            Url = new Uri("http://google.com")
+        },
+        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+        {
+            Name = "SRT Licence",
+            Url = new Uri("http://google.com")
+        },
+        TermsOfService = new Uri("http://google.com")
+    });
+
+    //- [Step 1: in setting of project->build->output->documentation file ✔]
+    //- [Step 2: in setting of project->build->output->XML documentation file Path ✔(examp: projectName.xml)]
+    //- [Code](https://github.com/SRTNT/SwaggerPractice/tree/main/src/SwaggerTest1)
+
+    var xmlCommentsfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsfilePath = Path.Combine(AppContext.BaseDirectory, xmlCommentsfile);
+
+    setup.IncludeXmlComments(xmlCommentsfilePath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(setup =>
+    {
+        setup.SwaggerEndpoint("/swagger/SRT/swagger.json", "SRT API UI");
+
+        // Remove swagger from url
+        setup.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
