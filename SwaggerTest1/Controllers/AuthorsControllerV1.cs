9eada0f6620354ff7b583ac7fc3e3291c837bbe4
SRT Mokaab <s.r.taheri@gmail.com>
2025-04-19 08:41:08 +0330

    Update

--- SwaggerTest1/Controllers/AuthorsControllerV1.cs
+++ SwaggerTest1/Controllers/AuthorsControllerV1.cs
@@ -4,13 +4,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Library.API.Controllers
 {
+    /// <summary>
+    /// sdfgsdfgsdfgsdfgsdfgs
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]    
     public class AuthorsV1Controller : ControllerBase
     {
+        /// <summary>
+        /// sdfgsdfgsdf
+        /// </summary>
         public AuthorsV1Controller()
         { }
 
+        /// <summary>
+        /// sdfgsdfgsd
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [ApiVersion("1.0")]
         public async Task<ActionResult<IEnumerable<string>>> func1()
@@ -18,9 +28,14 @@ namespace Library.API.Controllers
             return Enumerable.Range(1, 10).Select(index => "Authors" + index).ToArray();
         }
 
+        /// <summary>
+        /// سیبلسیبلایسبایبایبل
+        /// </summary>
+        /// <param name="authorId"></param>
+        /// <returns></returns>
         [HttpPost("{authorId}")]
         [ApiVersion("2.0")]
-        public async Task<ActionResult<string>> func2(int authorId)
+        public ActionResult<string> func2(int authorId)
         {
             if (authorId < 1 || authorId > 10)
             {
@@ -30,11 +45,15 @@ namespace Library.API.Controllers
             return Ok("Authors" + authorId);
         }
 
+        /// <summary>
+        /// esdfgsdfgsdfgsdf
+        /// </summary>
+        /// <returns></returns>
         [HttpPost]
         [Route("saeed")]
-        public async Task<ActionResult<string>> func3()
+        public Task<ActionResult<string>> Func3()
         {
-            return Ok("Authors" + 1);
+            return Task.FromResult<ActionResult<string>>(Ok("Authors" + 1));
         }
     }
 }
